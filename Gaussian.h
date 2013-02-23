d56f22a26dc72f78e20200bacd66b647da1741df
webgeist <webgeist.xor@gmail.com>
2013-02-24 00:02:23 +0400

    гауссиан

--- Gaussian.h
+++ Gaussian.h
@@ -16,6 +16,14 @@ public:
 	virtual ~Gaussian();
 	
 	IplImage* gaussianBlur( IplImage* const image, double sigma );
+	
+	IplImage* difference(IplImage * img1, IplImage * img2);
+
+	IplImage** difference_octave(IplImage ** octave, int scales);
+
+	IplImage*** difference_scale_space(IplImage *** scale_space,
+										int octaves,
+										int scales);
 private:
 
 };
